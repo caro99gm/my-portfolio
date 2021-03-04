5a39bbc2c4ac6d08c660e4ca65d8574a41c2e736
Carolina <caro99gm@gmail.com>
2021-03-04 07:26:10 +0000

    Servlets

--- portfolio/src/main/java/com/google/sps/MyServlet.java
+++ portfolio/src/main/java/com/google/sps/MyServlet.java
@@ -1,4 +1,4 @@
-package com.google.sps.servlets;
+package com.google.sps;
 
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -6,14 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/welcome")
-public class HelloWorldServlet extends HttpServlet {
-
+@WebServlet("/hello")
+public class MyServlet extends HttpServlet{
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     System.out.println("Arrives");
     response.getWriter().println("<h1>Welcome to my portfolio!</h1>");
-  }
+    }
 }
